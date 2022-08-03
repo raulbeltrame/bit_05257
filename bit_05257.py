c86a53ef4276c53d02e6a22b1ebf3840bec0c16e
raulbeltrame4 <raulbeltrame4@gmail.com>
2022-08-03 12:54:19 -0300

    adicionando comparacoes de sentencas simples

--- bit_05257.py
+++ bit_05257.py
@@ -11,6 +11,7 @@ while stop != "v":
 
     expression= str(input().strip())
 
+    #tratamento de erros
     if len(S1) != N or len(S2) != N:
         print("ERRO")
         stop ="v"
@@ -26,3 +27,61 @@ while stop != "v":
         print("ERRO")
         stop ="v"
         break
+
+    final = ["0" for i in range(N)]
+    #comparacoes
+    if expression == "S1 AND S2"  or expression == "S2 AND S1":
+        for i in range (N):
+            if S1[i] == "1" and S2[i] == "1":
+                final[i] = "1"
+            else:
+                final[i] = "0"
+
+    if expression == "S1 OR S2" or expression == "S2 OR S1":
+        for i in range(N):
+            if S1[i] == "0" and S2[i] == "0":
+                final[i] = "0"
+            else:
+                final[i] = "1"
+
+    if expression == "S1 XOR S2" or expression == "S2 XOR S1":
+        for i in range(N):
+            if S1[i] == S2[i]:
+                final[i] = "0"
+            else:
+                final[i] = "1"
+
+    if expression == "S1 NAND S2" or expression == "S2 NAND S1":
+        for i in range (N):
+            if S1[i] == "1" and S2[i] == "1":
+                final[i] = "0"
+            else:
+                final[i] = "1"
+
+    if expression == "S1 NOR S2" or expression == "S2 NOR S1":
+        for i in range (N):
+            if S1[i] == "0" and S2[i] == "0":
+                final[i] = "1"
+            else:
+                final[i] = "0"
+
+    if expression == "S1 MOR S2":
+        for i in range(N):
+            if S1[i] == "1" and S2[i] == "0":
+                final[i] = "0"
+            else:
+                final[i] = "1"
+
+    if expression == "S2 MOR S1":
+        for i in range(N):
+            if S2[i] == "1" and S1[i] == "0":
+                final[i] = "0"
+            else:
+                final[i] = "1"
+
+
+
+
+    for i in range(N):
+        print(final[i], end="")
+
